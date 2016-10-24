e1eb4e4773483e689aa4d09cfb16871a0fbd5f24
Perere128 <perere1995@gmail.com>
2016-10-24 21:00:37 +0100

    Add files via upload

---
+++ salinhabot-master/entrada.js
@@ -0,0 +1,13 @@
+API.on(API.USER_JOIN, function(user) {
+if (user.id == "14638563") {
+API.sendChat("/me Preparem-se, o martelo do BAN  @" + user.username + " acabou de entrar!.");
+}   
+
+if (user.id == "1380997") {
+API.sendChat("/me Preparem-se, o GOSTOSO  @" + user.username + " acabou de entrar!.");
+}  
+
+if (user.id == "4371581") {
+API.sendChat("/me o Gordinho Gostoso  @" + user.username + " acabou de entrar!.");
+} 
+});
